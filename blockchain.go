305211e03ac37aaf68a8b4a0705de00c99a9df00
uglo1 <yujiro.takahashi0912@gmail.com>
2022-03-20 06:18:37 +0900

    ✨ ブロック作成関数を作成

--- blockchain.go
+++ blockchain.go
@@ -3,8 +3,16 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 )
 
+func NewBlock(nonce int, previoutHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previoutHash
+	return b
+}
 func init() {
 	log.SetPrefix("Blockchain: ")
 }
