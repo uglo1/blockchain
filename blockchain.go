3a2d1c69b1af25d7a4caa1fa677a16e60ee40a71
uglo1 <yujiro.takahashi0912@gmail.com>
2022-03-20 07:25:15 +0900

    ✨ Blockchainのchain部分を表示するPrint関数を作成

--- blockchain.go
+++ blockchain.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -43,6 +44,14 @@ func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
 	b := NewBlock(nonce, previousHash)
 	bc.chain = append(bc.chain, b)
 	return b
+}
+
+func (bc *Blockchain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		block.Print()
+	}
+	fmt.Printf("%s\n", strings.Repeat("*", 25))
 }
 
 func init() {
