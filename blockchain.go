a9e416b549fe41b40b03e922cd31ffb937e37df1
uglo1 <yujiro.takahashi0912@gmail.com>
2022-03-20 08:24:55 +0900

    ✨ BlockにおけるpreviousHashのためのHash関数を作成

--- blockchain.go
+++ blockchain.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
@@ -27,6 +29,26 @@ func (b *Block) Print() {
 	fmt.Printf("nonce               %d\n", b.nonce)
 	fmt.Printf("previous_hash       %x\n", b.previousHash)
 	fmt.Printf("transactions        %s\n", b.transactions)
+}
+
+func (b *Block) Hash() [32]byte {
+	m, _ := json.Marshal(b)
+	return sha256.Sum256([]byte(m))
+}
+
+// publicな構造体で格納し、json.Marshalをオーバーライド
+func (b *Block) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Timestamp    int64    `json:"timestamp"`
+		Nonce        int      `json:"nonce"`
+		PreviousHash [32]byte `json:"previous_hash"`
+		Transactions []string `json:"transactions"`
+	}{
+		Timestamp:    b.timestamp,
+		Nonce:        b.nonce,
+		PreviousHash: b.previousHash,
+		Transactions: b.transactions,
+	})
 }
 
 type Blockchain struct {
