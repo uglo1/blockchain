9dea8fc772aac9a0ce493ceebd7455044c56b252
uglo1 <yujiro.takahashi0912@gmail.com>
2022-03-21 04:28:38 +0900

    ✨ Signatureの定義

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -100,3 +100,11 @@ func (w *Wallet) BlockchainAddress() string {
  * Signature
  */
 
+type Signature struct {
+	R *big.Int
+	S *big.Int
+}
+
+func (s *Signature) String() string {
+	return fmt.Sprintf("%x%x", s.R, s.S)
+}
