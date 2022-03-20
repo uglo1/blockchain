9a8308324d7680bed26fe6d02d5c88410d5a4bbe
uglo1 <yujiro.takahashi0912@gmail.com>
2022-03-20 23:14:41 +0900

    ✨ Transactionの確認用Print関数とjsonのMarshal関数を作成

--- blockchain.go
+++ blockchain.go
@@ -94,6 +94,25 @@ func NewTransaction(sender string, recipient string, value float32) *Transaction
 	return &Transaction{sender, recipient, value}
 }
 
+func (t *Transaction) Print() {
+	fmt.Printf("%s\n", strings.Repeat("-", 40))
+	fmt.Printf(" sender_blockchain_address        %s\n", t.senderBlockchainAddress)
+	fmt.Printf(" recipient_blockchain_address     %s\n", t.recipentBlockchainAddress)
+	fmt.Printf(" value                            %.1f\n", t.value)
+}
+
+func (t *Transaction) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Sender    string  `json:"sender_blockchain_address"`
+		Recipient string  `json:"recipient_blockchain_address"`
+		Value     float32 `json:"value"`
+	}{
+		Sender:    t.senderBlockchainAddress,
+		Recipient: t.recipentBlockchainAddress,
+		Value:     t.value,
+	})
+}
+
 func init() {
 	log.SetPrefix("Blockchain: ")
 }
