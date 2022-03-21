88855291dca48eec9e152c74d2bde5f0b16f6930
uglo1 <yujiro.takahashi0912@gmail.com>
2022-03-21 18:52:48 +0900

    ✨ サーバーのブロックチェーンを取得する関数を作成

--- blockchain_server/blockchain_server.go
+++ blockchain_server/blockchain_server.go
@@ -14,6 +14,8 @@ type BlockchainServer struct {
 	port uint16
 }
 
+var cache map[string]*block.Blockchain = make(map[string]*block.Blockchain)
+
 func NewBlockchainServer(port uint16) *BlockchainServer {
 	return &BlockchainServer{port}
 }
@@ -22,8 +24,16 @@ func (bcs *BlockchainServer) Port() uint16 {
 	return bcs.port
 }
 
-func HelloWorld(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Hello, World!")
+func (bcs *BlockchainServer) GetBlockchain() *block.Blockchain {
+	bc, ok := cache["blockchain"]
+	if !ok {
+		minersWallet := wallet.NewWallet()
+		bc := block.NewBlockchain(minersWallet.BlockchainAddress(), bcs.Port())
+		cache["blockchain"] = bc
+	}
+	return bc
+}
+
 }
 
 func (bcs *BlockchainServer) Run() {
