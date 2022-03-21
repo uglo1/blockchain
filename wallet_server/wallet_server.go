4f12adafbc9c2f7d08cccb50b80fb4412bd0b580
uglo1 <yujiro.takahashi0912@gmail.com>
2022-03-21 20:34:20 +0900

    ✨ transactionを作成するハンドラ関数を実装

--- wallet_server/wallet_server.go
+++ wallet_server/wallet_server.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"text/template"
 
+	"github.com/uglo1/blockchain/utils"
 	"github.com/uglo1/blockchain/wallet"
 )
 
@@ -53,8 +54,19 @@ func (ws *WalletServer) Wallet(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func (ws *WalletServer) CreateTransaction(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodPost:
+		io.WriteString(w, string(utils.JsonStatus("success")))
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println("ERROR: Invalid HTTP Method")
+	}
+}
+
 func (ws *WalletServer) Run() {
 	http.HandleFunc("/", ws.Index)
 	http.HandleFunc("/wallet", ws.Wallet)
+	http.HandleFunc("/transaction", ws.CreateTransaction)
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(ws.Port())), nil))
 }
