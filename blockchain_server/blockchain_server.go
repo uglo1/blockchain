cab9ae373849393b70a7ce16f16a4b84407e37da
uglo1 <yujiro.takahashi0912@gmail.com>
2022-04-06 11:57:38 +0900

    ✨ 自動マイニングAPIを作成

--- blockchain_server/blockchain_server.go
+++ blockchain_server/blockchain_server.go
@@ -130,10 +130,25 @@ func (bcs *BlockchainServer) Mine(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 	}
 }
+func (bcs *BlockchainServer) StartMine(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		bc := bcs.GetBlockchain()
+		bc.StartMining() // マイニングを行う
+
+		m := utils.JsonStatus("success")
+		w.Header().Add("Content-Type", "application/json")
+		io.WriteString(w, string(m[:]))
+	default:
+		log.Println("ERROR: Invalid HTTP Method")
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
 
 func (bcs *BlockchainServer) Run() {
 	http.HandleFunc("/", bcs.Getchain)
 	http.HandleFunc("/transactions", bcs.Transactions)
-	http.HandleFunc("/mine", bcs.Mine)
+	http.HandleFunc("/mine", bcs.Mine)            // 手動マイニング
+	http.HandleFunc("/mine/start", bcs.StartMine) // 自動マイニング
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(bcs.Port())), nil))
 }
