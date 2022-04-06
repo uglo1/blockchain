ffe47bcc4a6707bafa1f4df749b59fbdda579a96
uglo1 <yujiro.takahashi0912@gmail.com>
2022-04-06 11:14:39 +0900

    ✨ blockchain serverのtransactionsハンドラの作成

--- blockchain_server/blockchain_server.go
+++ blockchain_server/blockchain_server.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/uglo1/blockchain/block"
+	"github.com/uglo1/blockchain/utils"
 	"github.com/uglo1/blockchain/wallet"
 )
 
@@ -54,7 +56,62 @@ func (bcs *BlockchainServer) Getchain(w http.ResponseWriter, req *http.Request) 
 	}
 }
 
+func (bcs *BlockchainServer) Transactions(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		// blockchainを取得し、json型で出力する
+		w.Header().Add("Content-Type", "application/json")
+		bc := bcs.GetBlockchain()
+		transactions := bc.TransactionPool()
+		m, _ := json.Marshal(struct {
+			Transactions []*block.Transaction `json:"transactions"`
+			Length       int                  `json:"length"`
+		}{
+			Transactions: transactions,
+			Length:       len(transactions),
+		})
+		io.WriteString(w, string(m[:]))
+	case http.MethodPost:
+		// リクエストを構造体に格納をし、エラー処理
+		decoder := json.NewDecoder(req.Body)
+		var t block.TransactionRequest
+		err := decoder.Decode(&t)
+		if err != nil {
+			log.Printf("ERROR: %v", err)
+			io.WriteString(w, string(utils.JsonStatus("fail")))
+			return
+		}
+		if !t.Validate() {
+			log.Println("ERROR: missing field(s)")
+			io.WriteString(w, string(utils.JsonStatus("fail")))
+			return
+		}
+
+		// transactionの作成をし、statusを返す
+		publicKey := utils.PublicKeyFromString(*t.SenderPublicKey)
+		signagure := utils.SignatureFromString(*t.Signature)
+		bc := bcs.GetBlockchain()
+		isCreated := bc.CreateTransaction(*t.SenderBlockchainAddress,
+			*t.RecipientBlockchainAddress, *t.Value, publicKey, signagure)
+
+		w.Header().Add("Content-Type", "application/json")
+		var m []byte
+		if !isCreated {
+			w.WriteHeader(http.StatusBadRequest)
+			m = utils.JsonStatus("fail")
+		} else {
+			w.WriteHeader(http.StatusCreated)
+			m = utils.JsonStatus("success")
+		}
+		io.WriteString(w, string(m))
+	default:
+		log.Println("ERROR: Invalid HTTP Method")
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
 func (bcs *BlockchainServer) Run() {
 	http.HandleFunc("/", bcs.Getchain)
+	http.HandleFunc("/transactions", bcs.Transactions)
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(bcs.Port())), nil))
 }
